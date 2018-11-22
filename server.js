bd185a3b94286e1af624c1f764afc37875c4ad7f
kutyel <flavio.corparios@ext.soprasteria.com>
2018-11-22 14:51:39 +0100

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -14,12 +14,18 @@ server.route({
   method: 'GET',
   path: '/',
   handler: () => posts,
+  options: {
+    cors: true,
+  },
 })
 
 server.route({
   method: 'GET',
   path: '/comments/{postId}',
   handler: ({ params: { postId } }) => comments[postId],
+  options: {
+    cors: true,
+  },
 })
 
 const init = async () => {
